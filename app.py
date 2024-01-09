311ef598a9760315c8c2ea93392860e33aab1f5f
Ecrevisse <romeo.incardona@gmail.com>
2024-01-09 21:16:43 +0100

    final update

--- app.py
+++ app.py
@@ -14,16 +14,25 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
 
+from langchain.chains import LLMChain
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+)
+from langchain.memory import ConversationBufferMemory
+
 import tempfile
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
 questions = [
-    "question1 ?",
-    "question2 ?",
-    "question3 ?",
-    "question4 ?",
+    "Crée un modèle automatisé pour la programmation et la révision des horaires dans 12 salles de cinéma, prenant en compte les nouvelles sorties de films et visant à maximiser l'audience.",
+    "Élabore un modèle de rapport pour analyser la performance de nos films, en mettant l'accent sur le taux de location, la répartition des revenus avec les distributeurs, et une comparaison avec les performances des concurrents.",
+    "Rédige un modèle de communication pour informer les distributeurs des performances de leurs films dans nos cinémas, incluant des données sur la fréquentation et des commentaires sur l'accueil du public.",
+    "Évalue comment les données de wishlist, de préventes et de notes sur notre site pourraient être utilisées pour prédire la popularité et le succès des films auprès de différents segments de public.",
 ]
 
 
@@ -34,6 +43,40 @@ def prepare_file(uploaded_file):
         with open(path, "wb") as f:
             f.write(uploaded_file.getvalue())
     return path
+
+
+def agent_without_rag():
+    # LLM
+    llm = ChatOpenAI(
+        temperature=0,
+        model="gpt-4-1106-preview",
+        openai_api_key=api_key,
+    )
+
+    # Prompt
+    prompt = ChatPromptTemplate(
+        messages=[
+            SystemMessagePromptTemplate.from_template(
+                """
+                Tu es un assistant interactif et personnalisé pour la programmation de films chez Pathé Cinéma.
+
+                Assistant Programmation Pathé: est une IA conçue pour être un expert dans la programmation de films pour les cinémas Pathé, spécialisée dans l'analyse des tendances de box-office, la négociation avec les distributeurs de films, et la création de grilles de programmation efficaces. Cette IA possède une connaissance approfondie de l'industrie cinématographique, des stratégies de marketing de contenu, et des meilleures techniques de prévision des performances de films. Pour chaque question posée par l'utilisateur, le GPT répondra en posant une question en retour, pour personnaliser sa réponse en fonction des informations fournies par l'utilisateur. Chaque section présentée par le GPT sera suivie d'une question, permettant ainsi une interaction continue et une personnalisation poussée des stratégies de programmation fournies. Cette méthode garantit que chaque conseil est spécifiquement adapté aux besoins et objectifs du département de programmation de Pathé Cinéma, avec un focus particulier sur l'optimisation des programmations et la gestion efficace des contenus.
+                """
+            ),
+            # The `variable_name` here is what must align with memory
+            MessagesPlaceholder(variable_name="chat_history"),
+            HumanMessagePromptTemplate.from_template("{input}"),
+        ]
+    )
+
+    # Notice that we `return_messages=True` to fit into the MessagesPlaceholder
+    # Notice that `"chat_history"` aligns with the MessagesPlaceholder name
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conversation = LLMChain(llm=llm, prompt=prompt, verbose=True, memory=memory)
+    return conversation
+
+    # Notice that we just pass in the `question` variables - `chat_history` gets populated by memory
+    # conversation({"question": "hi"})
 
 
 def rag_tool_openai(filename: str):
@@ -60,27 +103,39 @@ def rag_tool_openai(filename: str):
     )
 
     context = """
-    Tu es un assistant interactif et personnalisé pour le recrutement sur LinkedIn
+    Tu es un assistant interactif et personnalisé pour la programmation de films chez Pathé Cinéma.
 
-    Assistant LinkedIn Recruiter: est une IA conçue pour être un expert en sourcing sur LinkedIn, spécialisé dans l'utilisation et la configuration de la licence recruiter LinkedIn pour les professionels du secteur bancaire. Cette IA maîtrise le marketing RH, la création de stratégie de marque employeur, ainsi que les meilleures techniques de rédaction. Pour chaque question posée par l'utilisateur, le GPT répondra en posant une question en retour, pour personnaliser sa réponse en fonction des informations fournies par l'utilisateur. Chaque section présentée par le GPT sera suivie d'une question, permettant ainsi une interaction continue et une personnalisation poussée des conseils fournis. Cette méthode garantit que chaque conseil est spécifiquement adapté aux besoins et objectifs de l'utilisateur, avec un focus particulier sur le secteur bancaire.
-    pour toute questions sur les formations tu proposera le lien clicable suivant : Romain CREMA : https://www.linkedin.com/in/romaincremamodernselling/ parmi les résultats
+    Assistant Programmation Pathé: est une IA conçue pour être un expert dans la programmation de films pour les cinémas Pathé, spécialisée dans l'analyse des tendances de box-office, la négociation avec les distributeurs de films, et la création de grilles de programmation efficaces. Cette IA possède une connaissance approfondie de l'industrie cinématographique, des stratégies de marketing de contenu, et des meilleures techniques de prévision des performances de films. Pour chaque question posée par l'utilisateur, le GPT répondra en posant une question en retour, pour personnaliser sa réponse en fonction des informations fournies par l'utilisateur. Chaque section présentée par le GPT sera suivie d'une question, permettant ainsi une interaction continue et une personnalisation poussée des stratégies de programmation fournies. Cette méthode garantit que chaque conseil est spécifiquement adapté aux besoins et objectifs du département de programmation de Pathé Cinéma, avec un focus particulier sur l'optimisation des programmations et la gestion efficace des contenus.
 
-    Tu peux chercher les informations dans le document.
+    Tu peux chercher les informations dans le document au besoin.
     """
     sys_message = SystemMessage(content=context)
 
     agent_executor = create_conversational_retrieval_agent(
-        llm, tools, system_message=sys_message, verbose=True
+        llm,
+        tools,
+        system_message=sys_message,
+        verbose=True,
     )
 
     return agent_executor
+
+
+def query(agent, question):
+    with st.spinner("Waiting for response..."):
+        response = agent({"input": question})
+        if "text" in response:
+            response = response["text"]
+        else:
+            response = response["output"]
+    return response
 
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-st.set_page_config(page_title="Assistant pour le recrutement sur LinkedIn")
+st.set_page_config(page_title="Assistant chatbot")
 
 st.markdown(
     """
@@ -94,20 +149,34 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-img_col0, _ = st.columns(2)
-img_col0.image(Image.open("static/pathé-white-logo-png.png"))
-st.title("Assistant pour le recrutement sur LinkedIn")
+# img_col0, _ = st.columns(2)
+left_co, cent_co, last_co = st.columns(3)
+with cent_co:
+    st.image(
+        Image.open("static/logo-international-white-low_res-scale-2_80x-PhotoRoom.png"),
+        width=300,
+    )
+st.title("Assistant chatbot")
 
-st.write("Please upload your PDF file below.")
+st.write("Selectionnez le PDF à analyser")
 
 file = st.file_uploader("Upload a pdf", type="pdf")
-if file is not None and (
-    "filename" not in st.session_state or file.name != st.session_state["filename"]
-):  # and "agent" not in st.session_state:
-    with st.spinner("Preparing file..."):
-        st.session_state["filename"] = file.name
-        file_path = prepare_file(file)
-        st.session_state.agent = rag_tool_openai(file_path)
+if "agent" not in st.session_state or (
+    file is not None
+    and (
+        "filename" not in st.session_state or file.name != st.session_state["filename"]
+    )
+):
+    with st.spinner("Preparing agent..."):
+        file_path = None
+        if file is not None:
+            st.session_state["filename"] = file.name
+            file_path = prepare_file(file)
+            st.session_state.agent = rag_tool_openai(file_path)
+        else:
+            st.session_state.agent = agent_without_rag()
+        st.session_state.messages = []
+        st.session_state.start = False
 
 # Display chat messages from history on app rerun
 if "messages" in st.session_state:
@@ -116,7 +185,7 @@ if "messages" in st.session_state:
             st.markdown(message["content"])
 
 st.markdown('<span id="button-after"></span>', unsafe_allow_html=True)
-if "agent" in st.session_state and "start" not in st.session_state:
+if "agent" in st.session_state and st.session_state.start == False:
     cols = st.columns(int(len(questions) / 2))
     for i, question in enumerate(questions):
         if cols[int(i / 2)].button(question):
@@ -124,8 +193,7 @@ if "agent" in st.session_state and "start" not in st.session_state:
             with st.chat_message("user"):
                 st.markdown(question)
             st.session_state.messages.append({"role": "user", "content": question})
-            with st.spinner("Waiting for response..."):
-                response = st.session_state.agent({"input": question})["output"]
+            response = query(st.session_state.agent, question)
             with st.chat_message("assistant"):
                 st.markdown(response)
             st.session_state.messages.append({"role": "assistant", "content": response})
@@ -141,8 +209,8 @@ if "agent" in st.session_state:
             st.markdown(prompt)
         # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.spinner("Waiting for response..."):
-            response = st.session_state.agent({"input": prompt})["output"]
+        response = query(st.session_state.agent, question)
+
 
 # Display assistant response in chat message container
 if "agent" in st.session_state:
